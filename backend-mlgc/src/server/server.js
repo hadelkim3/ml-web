5c9bc6bac6ecbbd52889bfbf7e462d1f0cfa36b0
hadelkim3 <153834598+hadelkim3@users.noreply.github.com>
2024-12-10 21:15:16 +0800

    Update server.js

--- backend-mlgc/src/server/server.js
+++ backend-mlgc/src/server/server.js
@@ -1,63 +1,49 @@
-/** @format */
+require('dotenv').config();
 
-require("dotenv").config();
-const Hapi = require("@hapi/hapi");
-const routes = require("../server/routes");
-const loadModel = require("../services/modelService");
-const InputError = require("../exceptions/InputError");
+
+const Hapi = require('@hapi/hapi');
+const routes = require('../server/routes');
+const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/InputError');
+
+
 
 (async () => {
-	const server = Hapi.server({
-		port: process.env.APP_PORT || 8080,
-		host: process.env.APP_HOST || "localhost",
-		routes: {
-			cors: {
-				origin: ["*"],
-			},
-			payload: {
-				maxBytes: 1 * 1024 * 1024,
-			},
-		},
-	});
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0',
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+        },
+    })
 
-	const model = await loadModel();
-	server.app.model = model;
+    const model = await loadModel();
+    server.app.model = model;
 
-	server.route(routes);
-
-	server.ext("onPreResponse", function (request, h) {
+    server.route(routes);  // Akan dibahas lebih lanjut setelah pembahasan extension.
+    server.ext('onPreResponse', function (request, h) {
         const response = request.response;
-        
-		if (response.isBoom && response.output.statusCode === 413) {
-			const newResponse = h.response({
-				status: "fail",
-				message: "Payload content length greater than maximum allowed: 1000000",
-			});
-			newResponse.code(413);
-			return newResponse;
-		}
-
-		if (response instanceof InputError) {
-			const newResponse = h.response({
-				status: "fail",
-				message: `${response.message}`,
-			});
-			newResponse.code(response.statusCode);
-			return newResponse;
-		}
-
-		if (response.isBoom) {
-			const newResponse = h.response({
-				status: "fail",
-				message: response.message,
-			});
-			newResponse.code(response.output.statusCode);
-			return newResponse;
-		}
-
-		return h.continue;
-	});
-
-	await server.start();
-	console.log(`Server start at: ${server.info.uri}`);
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message}`
+            })
+            newResponse.code(response.statusCode)
+            return newResponse;
+        }
+        if (response.isBoom) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message
+            });
+            newResponse.code(response.output.statusCode);
+            return newResponse;
+        }
+        return h.continue;
+    });
+ 
+    await server.start();
+    console.log(`Server start at: ${server.info.uri}`);
 })();
