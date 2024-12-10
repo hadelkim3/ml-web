f9d3ca07b9a02e79b1fc8ee4be2ff96cf18c219a
hadelkim3 <153834598+hadelkim3@users.noreply.github.com>
2024-12-10 21:14:54 +0800

    Update routes.js

--- backend-mlgc/src/server/routes.js
+++ backend-mlgc/src/server/routes.js
@@ -1,25 +1,24 @@
-/** @format */
-
-const { postPredictHandler, getPredictHandler } = require("../server/handler");
-
+const {postPredictHandler, predictHistories} = require('../server/handler');
+     
 const routes = [
-	{
-		path: "/predict",
-		method: "POST",
-		handler: postPredictHandler,
-		options: {
-			payload: {
-				allow: "multipart/form-data",
-				multipart: true,
-			},
-		},
-	},
-	{
-		path: "/predict/histories",
-		method: "GET",
-		handler: getPredictHandler,
-		options: {},
-	},
-];
-
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        maxBytes: 1000 * 1000,
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  },
+  {
+    path: '/predict/histories',
+    method: 'GET',
+    handler: predictHistories
+    
+  }
+]
+ 
 module.exports = routes;
