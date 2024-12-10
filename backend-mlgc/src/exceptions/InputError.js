94d5e1f1a97f162e4bd22ade69b87d7feab7b1b7
hadelkim3 <153834598+hadelkim3@users.noreply.github.com>
2024-12-10 21:12:10 +0800

    Update InputError.js

--- backend-mlgc/src/exceptions/InputError.js
+++ backend-mlgc/src/exceptions/InputError.js
@@ -1,13 +1,10 @@
-/** @format */
-
 const ClientError = require("../exceptions/ClientError");
-
+     
 class InputError extends ClientError {
-	constructor(message, statusCode = 400) {
-		super(message);
-        this.name = "InputError";
-        this.statusCode = statusCode;
-	}
+    constructor(message) {
+        super(message);
+        this.name = 'InputError';
+    }
 }
-
+ 
 module.exports = InputError;
