12ae3e42269b5961fb24697c67f1d1f2752a0a45
hadelkim3 <153834598+hadelkim3@users.noreply.github.com>
2024-12-12 17:43:26 +0800

    Update loadModel.js

--- backend-mlgc/src/services/loadModel.js
+++ backend-mlgc/src/services/loadModel.js
@@ -1,5 +1,17 @@
-const tf = require('@tensorflow/tfjs-node');
+/** @format */
+
+const tf = require("@tensorflow/tfjs-node");
+
 async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/cancer-predict-storag/model.json');
+	const modelPath = process.env.APP_ENV == "local" ? process.env.LOCAL_MODEL_URL : process.env.MODEL_URL;
+	console.log(`Trying to load model from: ${modelPath}`);
+
+	try {
+		return await tf.loadGraphModel('https://storage.googleapis.com/cancer-prediction-storge/model.json');
+	} catch (error) {
+		console.error("Error loading model:", error);
+		throw error;
+	}
 }
+
 module.exports = loadModel;
